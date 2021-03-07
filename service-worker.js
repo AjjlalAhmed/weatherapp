746eede7e97e3b0a73eb64d38a773e02813d907e
AjjlalAhmed <ajjlalahmed48@gmail.com>
2021-03-07 14:32:17 +0500

    upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.25d386ae988a0fbcf8f5ed0fc5b995b4.js"
+  "/precache-manifest.181f6066694838d5c5a1c2c673615772.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "weather-app"});
