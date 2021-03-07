4aecdfd5f30402a55ac5a3ba00954661888a9f56
AjjlalAhmed <ajjlalahmed48@gmail.com>
2021-03-07 15:06:12 +0500

    upload

---
+++ precache-manifest.f49d8e1000f0279852c37fa69c81b516.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d71e273f2cc2f8998c9",
+    "url": "/css/app.6b6f90ff.css"
+  },
+  {
+    "revision": "252393b22cd696738f6d724aa909f42d",
+    "url": "/img/bg.252393b2.png"
+  },
+  {
+    "revision": "de7bff6edf2d328c7c26de4a966e4123",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d71e273f2cc2f8998c9",
+    "url": "/js/app.7cc66246.js"
+  },
+  {
+    "revision": "66c3c73ba6cb11822fb9",
+    "url": "/js/chunk-vendors.75395513.js"
+  },
+  {
+    "revision": "04f03e59d9481f48cf5770bbfe7e1f94",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
